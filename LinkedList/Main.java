af18e8899dede99c374d963d4831f82e1e20f6c1
Suhyun Kim <Suhyun.Kim@viacommix.com>
2016-05-31 11:14:18 -0400

    Added the method that wasn't added.

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -14,7 +14,23 @@ public class Main {
         LinkedList newList;
         newList = list.reverseList();
         newList.printList();
+	
+	System.out.println(addNumbersFromTwoLinkedLists(list, newList));
+	
+    }
 
+    public int addNumbersFromTwoLinkedLists(LinkedList one, LinkedList two) {
+        if (one.isEmpty() && two.isEmpty()) {
+            return -1;
+        } else if (two.isEmpty()) {
+            return Integer.parseInt(convertNumbersFromLinkedListToString(one));
+        }
+        if (one.isEmpty()) {
+            return Integer.parseInt(convertNumbersFromLinkedListToString(two));
+        } else {
+            return Integer.parseInt(convertNumbersFromLinkedListToString(one)) +
+                    Integer.parseInt(convertNumbersFromLinkedListToString(two));
+        }
     }
 
     public String convertNumbersFromLinkedListToString(LinkedList list) {
